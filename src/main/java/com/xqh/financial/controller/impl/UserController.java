4627d20ffe36d8a26a4d561f64a3fb23a1f9c508
hsshhsshhssh <346731187@qq.com>
2017-05-05 22:51:50 +0800

    fix

--- src/main/java/com/xqh/financial/controller/impl/UserController.java
+++ src/main/java/com/xqh/financial/controller/impl/UserController.java
@@ -3,18 +3,24 @@ package com.xqh.financial.controller.impl;
 import com.xqh.financial.controller.api.IUserController;
 import com.xqh.financial.entity.PayUser;
 import com.xqh.financial.entity.dto.PayUserCreateDTO;
+import com.xqh.financial.entity.dto.PayUserUpdateDTO;
+import com.xqh.financial.entity.vo.PayUserVO;
 import com.xqh.financial.service.UserService;
 import com.xqh.financial.utils.CommonUtils;
 import com.xqh.financial.utils.DozerUtils;
+import com.xqh.financial.utils.Search;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import java.util.List;
 
 /**
  * Created by hssh on 2017/5/4.
@@ -40,5 +46,15 @@ public class UserController implements IUserController {
             CommonUtils.sendError(resp ,HttpServletResponse.SC_EXPECTATION_FAILED, "用户名重复" + user.getUsername());
         }
         return id;
+    }
+
+    @Override
+    public int update(@RequestBody @Valid PayUserUpdateDTO user) {
+        return 0;
+    }
+
+    @Override
+    public List<PayUserVO> queryList(@RequestParam("search") Search search, @RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "size", defaultValue = "10") @Max(1000) Integer size) {
+        return null;
     }
 }
