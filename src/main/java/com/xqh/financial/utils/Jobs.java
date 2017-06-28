02729eaf638f39f347c56c7050ffa32feba06b0b
hsshhsshhssh <346731187@qq.com>
2017-06-29 00:22:12 +0800

    关闭定时任务

--- src/main/java/com/xqh/financial/utils/Jobs.java
+++ src/main/java/com/xqh/financial/utils/Jobs.java
@@ -36,7 +36,7 @@ public class Jobs
     @Autowired
     PayUserSettlementMapper userSettlementMapper;
 
-    @Scheduled(cron = "0 0 1 * * ? ")
+    //@Scheduled(cron = "0 0 1 * * ? ")
     public void settlement()
     {
         int nowTime = (int) (System.currentTimeMillis()/1000);
