c741a79f9b8df4fd65aee4807ae8211f7c043262
hsshhsshhssh <346731187@qq.com>
2017-07-02 15:17:22 +0800

    通联支付宝支付

--- src/main/java/com/xqh/financial/controller/VSPPayController.java
+++ src/main/java/com/xqh/financial/controller/VSPPayController.java
@@ -7,69 +7,74 @@ import com.xqh.financial.utils.vsp.HttpConnectionUtil;
 import com.xqh.financial.utils.vsp.SybUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
 /**
  * Created by hssh on 2017/6/19.
  */
-@RestController
+//@RestController
+@Controller
 @RequestMapping("vsp")
 public class VSPPayController
 {
     private static Logger logger = LoggerFactory.getLogger(VSPPayController.class);
 
-    //@GetMapping("pay")
-    //public void pay(@RequestParam("money") int money, HttpServletRequest req, HttpServletResponse resp)
-    //{
-    //    try
-    //    {
-    //        //req.setAttribute("test", 123);
-    //        req.getRequestDispatcher("/vsp/pay/html").forward(req, resp);
-    //    }
-    //    catch (ServletException e)
-    //    {
-    //        e.printStackTrace();
-    //    } catch (IOException e)
-    //    {
-    //        e.printStackTrace();
-    //    }
-    //}
+    @GetMapping("pay/html")
+    public void payHtml(@RequestParam("money") int money, HttpServletRequest req, HttpServletResponse resp)
+    {
+        try
+        {
+            String payUrl = getPayUrl(money);
+
+            req.setAttribute("payUrl", payUrl);
+            req.getRequestDispatcher("/vsp/html").forward(req, resp);
+        }
+        catch (ServletException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("html")
+    public String html(HttpServletRequest req, HttpServletResponse resp, ModelMap modelMap)
+    {
+
+        modelMap.addAttribute("payUrl", req.getAttribute("payUrl"));
+
+        return "index";
+    }
+
 
     @GetMapping("pay")
-    public void payJsp(HttpServletRequest req, HttpServletResponse resp, @RequestParam("money") int money) throws Exception {
+    @ResponseBody
+    public void payJsp(HttpServletRequest req, HttpServletResponse resp, @RequestParam("money") int money)
+    {
 
-        HttpConnectionUtil http = new HttpConnectionUtil("https://vsp.allinpay.com/apiweb/unitorder"+"/pay");
-        http.init();
-        TreeMap<String, String> params = Maps.newTreeMap();
+        String payUrl = getPayUrl(money);
 
-        params.put("cusid", "335581048165186");
-        params.put("appid", "00010835");
-        params.put("version", "11");
-        params.put("trxamt", String.valueOf(money));
-        params.put("reqsn", String.valueOf(System.currentTimeMillis()));
-        params.put("paytype", "A01");
-        params.put("randomstr", SybUtil.getValidatecode(8));
-        params.put("body", "测试应用");
-        params.put("remark", "mark");
-        //params.put("acct", acct);
-        params.put("notify_url", "http://139.196.51.152:8080/vsp/callback");
-        params.put("sign", SybUtil.sign(params,"gzxqhpay1qa@WS#ED"));
-
-        byte[] bys = http.postParams(params, true);
-        String result = new String(bys,"UTF-8");
-        Map<String,String> map = SybUtil.handleResult(result);
-        String payUrl = map.get("payinfo");
-
-        resp.sendRedirect(payUrl);
+        try
+        {
+            resp.sendRedirect(payUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
     @PostMapping("callback")
+    @ResponseBody
     public void callback(HttpServletRequest req, HttpServletResponse resp)
     {
         TreeMap<String, String> params = SybUtil.getParams(req);
@@ -79,6 +84,7 @@ public class VSPPayController
     }
 
     @PostMapping("notify")
+    @ResponseBody
     public void notify(HttpServletRequest req, HttpServletResponse resp)
     {
         Map<String, String[]> params = req.getParameterMap();
@@ -91,6 +97,40 @@ public class VSPPayController
             }
         }
         logger.info("/mch/notify param: " + queryString.substring(0, queryString.length() - 1));
+    }
+
+    public static String getPayUrl(int money)
+    {
+        try {
+            HttpConnectionUtil http = new HttpConnectionUtil("https://vsp.allinpay.com/apiweb/unitorder"+"/pay");
+            http.init();
+            TreeMap<String, String> params = Maps.newTreeMap();
+
+            params.put("cusid", "335581048165186");
+            params.put("appid", "00010835");
+            params.put("version", "11");
+            params.put("trxamt", String.valueOf(money));
+            params.put("reqsn", String.valueOf(System.currentTimeMillis()));
+            params.put("paytype", "A01");
+            params.put("randomstr", SybUtil.getValidatecode(8));
+            params.put("body", "测试应用");
+            params.put("remark", "mark");
+            //params.put("acct", acct);
+            params.put("notify_url", "http://139.196.51.152:8080/vsp/callback");
+            params.put("sign", SybUtil.sign(params,"gzxqhpay1qa@WS#ED"));
+
+            byte[] bys = http.postParams(params, true);
+            String result = new String(bys,"UTF-8");
+            Map<String,String> map = SybUtil.handleResult(result);
+            String payUrl = map.get("payinfo");
+
+            return payUrl;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
