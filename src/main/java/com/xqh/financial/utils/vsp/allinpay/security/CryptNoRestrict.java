a75775fd1f288416860ea565c3b89773ff3cf280
hsshhsshhssh <346731187@qq.com>
2017-07-06 00:53:23 +0800

    demo文件修改

--- src/main/java/com/xqh/financial/utils/vsp/allinpay/security/CryptNoRestrict.java
+++ src/main/java/com/xqh/financial/utils/vsp/allinpay/security/CryptNoRestrict.java
@@ -3,8 +3,6 @@
  */
 package com.xqh.financial.utils.vsp.allinpay.security;
 
-import org.springframework.util.ResourceUtils;
-
 import java.io.FileInputStream;
 import java.security.KeyStore;
 import java.security.Provider;
@@ -68,8 +66,8 @@ public class CryptNoRestrict implements CryptInf
 		this.lastSignMsg = "";
 		KeyStore ks = prvd==null?KeyStore.getInstance("PKCS12"):KeyStore.getInstance("PKCS12",prvd);
 		// ks.load(new FileInputStream(KeyFile), PassWord.toCharArray());
-		//fiKeyFile = new FileInputStream(KeyFile);
-		fiKeyFile = new FileInputStream(ResourceUtils.getFile("classpath:file/20058100001175504.p12"));
+		fiKeyFile = new FileInputStream(KeyFile);
+		//fiKeyFile = new FileInputStream(ResourceUtils.getFile("classpath:file/20058100001175504.p12"));
 		//PassWord.toCharArray()
 		try
 		{
