d528a7794df238db18c7be37c45657449424ba80
hsshhsshhssh <346731187@qq.com>
2017-07-22 23:57:39 +0800

    热云

---
+++ src/main/java/com/xqh/financial/controller/ReYunADDemoController.java
@@ -0,0 +1,60 @@
+package com.xqh.financial.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.xqh.financial.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.TreeMap;
+
+/**
+ * Created by hssh on 2017/7/22.
+ */
+@RestController
+@RequestMapping("/reyun")
+public class ReYunADDemoController
+{
+
+    private static Logger logger = LoggerFactory.getLogger(ReYunADDemoController.class);
+
+
+    @GetMapping("ad")
+    public void ad(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String idfa = "D5EAC54B-8A97-46B6-A257-95392C46C325";
+        String mac = "90:B9:31:58:AE:80";
+        String muid = CommonUtils.getMd5(idfa);
+        String callback = URLEncoder.encode("http://139.196.51.152:8080/nodifyUrl","UTF-8");
+
+
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("http://uri6.com/ZNVV3m");
+        sb.append("?idfa=" + idfa);
+        sb.append("&mac=" + mac);
+        sb.append("&muid=" + muid);
+        sb.append("&clickid=clickid");
+        sb.append("&callback=" + callback);
+
+        logger.info("热云联盟 url: {}", sb.toString());
+
+        resp.sendRedirect(sb.toString());
+
+    }
+
+    @GetMapping("callback")
+    public void callback(HttpServletRequest req, HttpServletResponse resp)
+    {
+        TreeMap<String, String> params = CommonUtils.getParams(req);
+        logger.info("热云 callback params:{}", JSONObject.toJSON(params));
+    }
+
+}
