1e603637183fecc3555308250d593138b5cdb303
hsshhsshhssh <346731187@qq.com>
2017-09-03 21:28:44 +0800

    fix

--- src/main/java/com/xqh/financial/utils/jobs/SettlementJobs.java
+++ src/main/java/com/xqh/financial/utils/jobs/SettlementJobs.java
@@ -13,6 +13,7 @@ import com.xqh.financial.utils.Search;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.entity.Example;
 
