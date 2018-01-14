9126ddea8ded2c9ed3a569d33338fe5ddc969832
hsshhsshhssh <346731187@qq.com>
2018-01-14 20:47:00 +0800

    '威富通'

---
+++ src/main/java/com/xqh/financial/controller/api/IPayWFTPayController.java
@@ -0,0 +1,22 @@
+package com.xqh.financial.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by hssh on 2018/1/14.
+ */
+@RequestMapping("/xqh/financial/wft")
+public interface IPayWFTPayController
+{
+
+    /**
+     * 威富通异步回调地址
+     * @param req
+     * @param resp
+     */
+    @RequestMapping("/pay/callback")
+    public void callback(HttpServletRequest req, HttpServletResponse resp);
+}
--- src/main/java/com/xqh/financial/utils/Constant.java
+++ src/main/java/com/xqh/financial/utils/Constant.java
@@ -112,6 +112,11 @@ public class Constant {
      */
     public static String RUIXUN_CHANNEL_CODE = "0003";
 
+    /**
+     * 威富通支付通道
+     */
+    public static String WFT_CHANNEL_CODE = "0004";
+
 
 
 ////////////////////////////////////////////////////////////////////////
